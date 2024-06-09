a3ebaa4cf3079fa3dad4d05b691ac1039e360631
nadiawaseem <161966596+Nadiawaseem@users.noreply.github.com>
2024-06-08 17:16:12 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var userName = ["admin", "ali", "waseem", "nadim", "adnan"];
+if (userName.length === 0) {
+    console.log("we need to find some user");
+}
+else {
+    userName = [];
+    console.log("all user has been removed", userName.length);
+}
---
+++ main.ts
@@ -0,0 +1,10 @@
+  let userName :string[] = ["admin","ali","waseem","nadim","adnan"];
+
+  if(userName.length === 0){
+    console.log("we need to find some user");
+
+  }else{
+    userName = [];
+    console.log("all user has been removed",userName.length);
+
+  }
